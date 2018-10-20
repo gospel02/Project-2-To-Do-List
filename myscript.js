9787ab53116da24cc13b885cba54655cbe34048a
Casey Randall <42552122+gospel02@users.noreply.github.com>
2018-10-20 13:45:07 -0600

    Changed back to the old layout

--- myscript.js
+++ myscript.js
@@ -59,7 +59,7 @@ function addlist(){
 }
 
 function addEmptyTask(element) {
-    $(element).parent().append("<div class='item'>"+
+    $(element).parent().after("<div class='item'>"+
         "<i id='square' class='check far fa-square'></i>"+
         "<input class='newitem' contenteditable='true' placeholder='Add Item'></input>"+
         "<i class='handle fas fa-arrows-alt-v'></i>"+
